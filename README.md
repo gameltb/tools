847521f8d03fe8c4b292b3beb1939bf1f06fd94d
gameltb <gamegccltb@gmail.com>
2020-11-18 22:58:22 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tools
+&emsp;&emsp;一些脚本小工具
+
+## zip_gbk.py
+&emsp;&emsp;解压文件名里带有gbk编码字符的zip文件。  
+&emsp;&emsp;如果有多个文件，会将文件放进一个文件夹里。
+```shell
+./zip_gbk.py <zip文件>
+```
---
+++ zip_gbk.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import zipfile
+
+
+def main(filename):
+    with zipfile.ZipFile(filename, "r") as thiszipfile:
+        base = os.path.dirname(filename)
+        if len(thiszipfile.namelist()) > 1:
+            base, _ = os.path.splitext(filename)
+        for name in thiszipfile.namelist():
+            utf8name = os.path.join(base, name.encode("cp437").decode('gbk'))
+            print("Extracting " + utf8name)
+            pathname = os.path.dirname(utf8name)
+            if not os.path.exists(pathname) and pathname != "":
+                os.makedirs(pathname)
+            data = thiszipfile.read(name)
+            if not os.path.exists(utf8name):
+                with open(utf8name, "wb") as fo:
+                    fo.write(data)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
